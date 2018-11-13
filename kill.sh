1c29bde373da770e56ef184958ad9073ce9c473b
SunnyQue <qjm3662@mail.dlut.edu.cn>
2018-11-13 10:33:58 +0800

    update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kill `ps -aux | grep CCNController-1.0.jar | awk '{print $2}'`
-kill `ps -aux | grep CCNRouter-1.0.jar | awk '{print $2}'`
+kill `ps -aux | grep CCNController-1.0.jar | grep -v grep | awk '{print $2}'`
+kill `ps -aux | grep CCNRouter-1.0.jar | grep -v grep | awk '{print $2}'`
