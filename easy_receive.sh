1013170e2027087bdd4f919ecb2e2babaa04406f
SunnyQue <qjm3662@mail.dlut.edu.cn>
2018-11-13 16:51:12 +0800

    update

--- easy_receive.sh
+++ easy_receive.sh
@@ -5,7 +5,7 @@ num=$2
 for idx in `seq 1 $num`
 do
     #echo $fileName\#\$\#$idx
-    exec ndncatchunks $fileName\#\$\#$idx &
+    exec ndncatchunks /home/node0/Documents/CCNDeployment/xu/$fileName\#\$\#$idx &
 done
 
 wait
