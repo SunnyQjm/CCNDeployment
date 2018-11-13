ce304d0afc65dce3d02857cfa137787c9d137990
SunnyQue <qjm3662@mail.dlut.edu.cn>
2018-11-13 16:08:22 +0800

    update

--- uploadLogToBlockChain.sh
+++ uploadLogToBlockChain.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-scp -i ~/pem/PKUsz1.pem log.ini root@60.205.191.3:/root/packages/PoV-DN-version/phxpaxos_test/Debug/Domain_name_list
-cat /dev/null > log.ini
+
+# 首先判断log.ini是否不为空，不为空才执行上传操作
+if [ -s log.ini ]; then
+    scp -i ~/pem/PKUsz1.pem log.ini root@60.205.191.3:/root/packages/PoV-DN-version/phxpaxos_test/Debug/Domain_name_list
+    cat /dev/null > log.ini
+fi
