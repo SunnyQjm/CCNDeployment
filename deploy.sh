13024752825122cfeba1042db8ae8ed4d5965bd4
SunnyQue <qjm3662@mail.dlut.edu.cn>
2018-11-13 11:28:29 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ spawn ssh $username@$ip "
 cd Documents/CCNDeployment;
 git pull;
 nohup /home/$username/software/jdk11/bin/java -jar ./CCNController-1.0.jar $routerName > controller_log.txt &
-sleep 12;
+sleep 20;
 nohup /home/$username/software/jdk11//bin/java --class-path ./CCNRouter-1.0.jar:. cn.pkusz.run.RouterTest $routerName > router_log.txt & 
 echo $routerName deploy finish!!
 "
